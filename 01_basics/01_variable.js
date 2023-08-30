bfc8230b1d810e3650c13dade28c7303b3fa857a
AHTASHAM NAEEM <141627116+ahtasham2001@users.noreply.github.com>
2023-08-30 11:44:20 +0000

    variable basic notes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 0;
+let accountEmail = "ahtasham@google.com";
+var accountPassword = "12345";
+accountCity = "Multan";
+
+// accountId = 2 not allowed 
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope.
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
