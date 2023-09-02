2f1ad9c16058e2789a9d74088c0a7f9f89740c4d
AHTASHAM NAEEM <141627116+ahtasham2001@users.noreply.github.com>
2023-09-02 14:56:08 +0000

    number_math

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,33 @@
+let score = 400;
+console.log(score);
+
+let balance = new Number (700);
+console.log(balance);
+console.log(score.toFixed());
+console.log(balance.toFixed());
+console.log(score.toPrecision(3))
+console.log(score.toPrecision(4))
+console.log(score.toPrecision(2))
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
